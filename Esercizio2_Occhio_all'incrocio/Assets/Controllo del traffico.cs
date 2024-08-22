b47ebed875db0fa37defe955fe3b152eaad89323
Scoobyz95 <ragazzosamuele7@gmail.com>
2024-08-23 00:14:17 +0200

    Traffico
    Ho aggiunto la questione dei semafori, delle macchine che si fermano e accelerano, ho iniziato la parte grafica dei semafori che si illimuna (rosso,giallo,verde),ho creato la terra perchè voglio dare una fisica alle macchine, così quando ci vai addosso si spostano di più dei camion o autobus. Pontarolo usa i modelli che ho fatto, sopratutto i semafori!!

--- Esercizio2_Occhio_all'incrocio/Assets/Controllo del traffico.cs
+++ Esercizio2_Occhio_all'incrocio/Assets/Controllo del traffico.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,20 +8,22 @@ public class Controllodeltraffico : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject[] macchine;
+    Stopwatch timersemafori;
+    public List<GameObject> semafori = new List<GameObject>();
+    public List<GameObject> semafori_segnali = new List<GameObject>();
     //diventerà un input
-    float speed = 5f;
-    float decelerazionesemaforo = 10f;
-    float decelerazioneautodavanti = 5f;
+    float speed = 8f;
+    float decelerazionesemaforo = 15f;
+    float decelerazioneautodavanti = 30f;
+    float accelerazione = 15f;
+    
     float[] velocita;
+    
 
     // bisogna sistemare la questione degli assi
     void Start()
     {
         macchine = GameObject.FindGameObjectsWithTag("Macchine");
-        //for (int i = 0; i < macchine.Length; i++)
-        //{
-        //    StartCoroutine(MuoviLaMacchina(macchine[i]));
-        //}
         velocita = new float[macchine.Length];
 
         for (int i = 0; i < macchine.Length; i++)
@@ -28,82 +31,144 @@ public class Controllodeltraffico : MonoBehaviour
             velocita[i] = speed;
         }
 
+        timersemafori = new Stopwatch();
+        timersemafori.Start();
+
     }
 
     // Update is called once per frame
+    // 0 avanti = forward
+
+    // 180 avanti = -forward
+    //Questo è il giallo
+    bool giallo = false;
+    int cont = 1;
+    float tempo = 10;
+
+
+    public float emissionIntensity = 5.0f; // Intensità dell'emissione
+    Material sferadailluminare;
+
+    bool rossooverde = true;
     void Update()
     {
+        int layerMaskpassi = 1 << 6;
         int layerMasknonpassi = 1 << 7;
         int layerMaskveicolo = 1 << 8;
 
         
         for (int i = 0; i < macchine.Length; i++)
         {
-            Vector3 avanti = -Vector3.forward;
+            Vector3 avanti = macchine[i].transform.forward ;
 
 
-            Debug.DrawRay(macchine[i].transform.position, avanti, Color.red, 1f);
+            //Debug.DrawRay(macchine[i].transform.position, avanti * 14f, Color.red);
 
 
-            if (Physics.Raycast(macchine[i].transform.position, avanti, 10000f, layerMasknonpassi))
+            if (Physics.Raycast(macchine[i].transform.position, avanti, 14f, layerMasknonpassi))
             {
+              
                 
-                // Decelerazione graduale
-                velocita[i] -= decelerazionesemaforo * Time.deltaTime;
-                if ( velocita[i] < 3 ) velocita[i] = 0;
+                    if (velocita[i] > 0)
+                        Rallenta(i, decelerazionesemaforo);
+                
             }
-            else if(Physics.Raycast(macchine[i].transform.position, avanti, 3f, layerMaskveicolo))
+            else if (Physics.Raycast(macchine[i].transform.position, avanti, 8f, layerMaskveicolo))
             {
+                if (velocita[i] > 0)
+                Rallenta(i, decelerazioneautodavanti);
                 
-                velocita[i] -= decelerazioneautodavanti * Time.deltaTime;
-                if (velocita[i] < 0) velocita[i] = 0;
+            }
+            else if (Physics.Raycast(macchine[i].transform.position, avanti, 14f, layerMaskpassi))
+            {
+                if (timersemafori.Elapsed.TotalSeconds + 3 > tempo * cont)
+                {
+                    if (velocita[i] > 0)
+                        Rallenta(i, decelerazionesemaforo + 25f);
+                }
+                Accelera(i, accelerazione);
+
+
+            }
+            else
+            {
+
+                Accelera(i, accelerazione);
+
             }
 
-            macchine[i].transform.Translate(Vector3.forward *velocita[i] * Time.deltaTime);
+            macchine[i].transform.Translate(Vector3.forward * velocita[i] * Time.deltaTime);
+        }
+
+        if (rossooverde)
+        {
+            // da continuare
+            //Verde
+            //sferadailluminare = semafori_segnali[0]
+        }
+
+
+        if (timersemafori.Elapsed.TotalSeconds > tempo * cont)
+        {
+            cont++;
+            foreach (GameObject semaforo in semafori)
+            {
+                semaforo.transform.Rotate(Vector3.up, 90);
+
+                
+            }
+
+            
+           
+        }
+        //else if (timersemafori.Elapsed.TotalSeconds > tempo * cont + 2)
+        //{
+        //    cont++;
+        //    foreach (GameObject semaforo in semafori)
+        //    {
+        //        semaforo.transform.Rotate(Vector3.up, -45);
+        //        giallo = false;
+        //    }
+
+        //}
+    }
+
+    void Rallenta(int i, float decelerazione)
+    {
+        velocita[i] -= decelerazione * Time.deltaTime;
+        if (velocita[i] < 0.1f)
+        {
+            velocita[i] = 0;
+            Rigidbody rb = macchine[i].GetComponent<Rigidbody>();
+            rb.velocity= Vector3.zero;
         }
     }
 
-    //IEnumerator Decelera(float speedattuale, int i)
-    //{
-    //    while (speedattuale > 0)
-    //    {
-    //        speedattuale -= decelerazione * Time.deltaTime;
-    //        macchine[i].transform.Translate(Vector3.forward * speedattuale * Time.deltaTime);
-    //        yield return null;
-    //    }
-    //    speedattuale = 0;
-    //}
-    // IEnumerator MuoviLaMacchina(GameObject macchina)
-    //{
-    //    int layerMasknonpassi = 1 << 7;
-    //    int layerMaskveicolo = 1 << 8;
-    //    float speedattuale = speed;
-      
-        
-    //    while (true)
-    //    {
-    //        // Controlla se c'è un ostacolo davanti
-    //        Debug.DrawRay(macchina.transform.position, macchina.transform.forward, Color.red, 20f);
+    void Accelera(int i, float accelerazione)
+    {
+        if (velocita[i] < 8)
+        {
+            Rigidbody rb = macchine[i].GetComponent<Rigidbody>();
 
-    //        if (Physics.Raycast(macchina.transform.position, macchina.transform.forward, 1.5f, layerMasknonpassi) ||
-    //            Physics.Raycast(macchina.transform.position, macchina.transform.forward, 1.5f, layerMaskveicolo))
-    //        {
-    //            // Decelerazione graduale
-    //            while (speedattuale > 0)
-    //            {
-    //                speedattuale -= decelerazione * Time.deltaTime;
-    //                macchina.transform.Translate(Vector3.forward * speedattuale * Time.deltaTime);
-    //                yield return null;  // Attende il prossimo frame
-    //            }
-    //            speedattuale = 0;  // Assicura che la velocità non vada sotto minSpeed
-    //        }
-    //        else
-    //        {
-    //            macchina.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    //        }
+            if (velocita[i] == 0)
+            {
+                
+                velocita[i] += accelerazione * Time.deltaTime;
+                rb.AddForce(macchine[i].transform.forward * velocita[i], ForceMode.VelocityChange);
+                
+            }
+            else
+            {
+                velocita[i] += accelerazione * Time.deltaTime;
 
-    //        // Attende il prossimo frame
-    //        yield return null;
-    //    }
-    //}
+                if (velocita[i] > 8)
+                {
+                    velocita[i] = 8;
+                }
+            }
+
+        }
+    }
+
+
 }
